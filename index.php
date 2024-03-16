523c86db63c8b810a7894658cdfb005e8ae8386a
chandachewe10 <chewec03@gmail.com>
2024-03-16 19:58:10 +0200

    Added template Messaging Unit Test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 use Chandachewe\Whatsapp\Messages\TemplateMessage;
 
  // Prepare    
-$templateMessage = new TemplateMessage("v19.0", "103211462576623", "260769891754", "EAALG7oWrKU4BOZCXMJqbSgW2Pe9AFnOXleIVTrhjFGWVAlLo43ShC5yhd6BoLH8TcSEZCTQvMmLn4ouz4Knjwf1kwByfUmichwE9X3ZAXaI7QZCdLNCeFtXQALZBjakORNiJtYBSzeiYet3LxNhNgc6Blmd182JmQsQTMULmZAIZBfZAIhQCqCIJbhqjou5nx9EQ8zDt2Iw5MXLVIwusI8gZD");
+$templateMessage = new TemplateMessage("v19.0", "", "260769891754", "");
 
  // Act
 $response = $templateMessage->template('hello_world','en_US');
