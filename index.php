573265426f3256e9f00cfe5ab8c80c0ec271eb49
chandachewe10 <chewec03@gmail.com>
2024-03-17 23:58:02 +0200

    Add List Message Class

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 require_once 'vendor/autoload.php';
-use Chandachewe\Whatsapp\Messages\TemplateMessage;
+use Chandachewe\Whatsapp\Messages\TextMessage;
 
- // Prepare    
-$templateMessage = new TemplateMessage("v19.0", "", "260769891754", "");
+ 
+$templateMessage = new TextMessage("v19.0", "103211462576623", "260769891754", "EAALG7oWrKU4BO37Sbq125BME9EZA6sp6LgblGaKjCjaQF8yQM2QZCFZAJL6CJ0lsfj16Gqm02Fyafo8uZChV5iT1rcAOvpNdRgOnm8PJ7GWEyVZCQolkc4UP0AZBgWbxN6vzO2E2F6FrEfpdERt8yT94pFd1qJowLvg4tixNiSoQXnZCTj9XIwidO6ZCnMCewwe1IPk7rbcmUE7ZBKdbTRH4S");
 
- // Act
-$response = $templateMessage->template('hello_world','en_US');
+ 
+$response = $templateMessage->text('hello_world');
 
 echo $response;
-// Assert
+
 
 
