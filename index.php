059df7df391f744acbd42101eea83949b8c63e0b
chandachewe10 <chewec03@gmail.com>
2024-03-18 09:12:45 +0200

    Added Location Message

--- index.php
+++ index.php
@@ -1,42 +1,23 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use Chandachewe\Whatsapp\Messages\ButtonMessage;
+use Chandachewe\Whatsapp\Messages\LocationMessage;
 
-$templateMessage = new ButtonMessage( 'v19.0',
+
+$templateMessage = new LocationMessage( 'v19.0',
 '103211462576623',
 '260769891754',
 'EAALG7oWrKU4BO8sT6zUXLt3tYI3GKvqBjpX2hQDCVQCBk4ZBiTc8KJT7LKVdYYPZBLFkyuhD6fWkHw8uYEHtfyuAYxjudXbCQoYKeIJVdF0n5d3qarQ2M9Qa8vqZAVkX2erFI4JrErmYIGXZBQarO1H6LWvLnTG4uBFPVqaJ1bdg9bZAR2W0UhG2CGYoJ6ZAIhMSUkkTNZBcPaOBZA2VbSMZD');
 
-$response = $templateMessage->button(   
-    'List of Cities',
-    'Please select the city where you are coming from','',
+$response = $templateMessage->location(   
+    '-28.28882772',
+    '35.2883883',
+    'Lusaka',
+    'Northmead',    
+    'Please select the city where you are coming from on the map','',
     [
-        'buttons' => [
-            [
-                'type' => 'reply',
-                'reply' => [
-                    'id' => 'unique-postback-id',
-                    'title' => 'Mtn'
-                ]
-            ],
-            [
-                'type' => 'reply',
-                'reply' => [
-                    'id' => 'unique-postback2-id',
-                    'title' => 'Airtel'
-                ]
-            ],
-            [
-                'type' => 'reply',
-                'reply' => [
-                    'id' => 'unique-postback3-id',
-                    'title' => 'Zamtel'
-                ]
-            ]
-        ]
-    ]
-    
+    'name' => 'send_location'
+    ]   
 );
 
 echo $response;
---
+++ src/Messages/LocationMessage.php
@@ -0,0 +1,71 @@
+<?php
+namespace Chandachewe\Whatsapp\Messages;
+
+require '.../../vendor/autoload.php';
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv->load();
+use GuzzleHttp\Client;
+
+class LocationMessage
+{
+    private $version;
+    private $businessPhoneNumberId;
+    private $recipientNumber;
+    private $token;
+
+    public function __construct(string $version, $businessPhoneNumberId, $recipientNumber, string $token)
+    {
+        $this->version = $version;
+        $this->businessPhoneNumberId = sprintf("%.0f", $businessPhoneNumberId);
+        $this->recipientNumber = sprintf("%.0f", $recipientNumber);
+        $this->token = $token;
+    }
+
+
+    public function location(string $longitude,string $latitude, string $nameOfLocation, string $address, string $body, string $footer = null,$sections)
+    {
+        try {
+
+
+            $data = [
+                'messaging_product' => 'whatsapp',
+                'recipient_type' => 'individual',
+                'to' => $this->recipientNumber,
+                'type' => 'LOCATION',
+                'location' => [
+                    'longitude' => $longitude,
+                    'latitude' => $latitude,
+                    'name' => $nameOfLocation,
+                    'address' => $address
+                ],
+                'body' => [
+                    'type' => 'text',
+                    'text' => $body
+                ],
+                'footer' => [
+                    'text' => $footer
+                ],
+                'action' => $sections
+            ];
+            
+            
+            $client = new Client();
+            $jsonData = json_encode($data);
+            $response = $client->post($_ENV['BASE_URI'] . '/' . $this->version . '/' . $this->businessPhoneNumberId . '/messages', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token,
+                    'Content-Type' => 'application/json',
+
+                ],
+                'body' => $jsonData
+            ]);
+
+
+            $responseBody = $response->getBody();
+            return $responseBody;
+
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
